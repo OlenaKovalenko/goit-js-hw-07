19b18c024f089658459145b116e085a0a57dedce
OlenaKovalenko <elenkow17@gmail.com>
2023-08-31 22:39:59 +0300

    Create markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const container = document.querySelector(".gallery");
+ const markup = createMarkup(galleryItems)
+  
+  container.insertAdjacentHTML('beforeend', markup);
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    }).join("")
+};
+
+
+
 console.log(galleryItems);
